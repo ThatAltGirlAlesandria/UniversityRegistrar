43729e9863a79d8f636c98dfa42facd527fe1a4b
Thomas Bakken <tbakken14@gmail.com>
2023-08-01 13:48:38 -0700

    Add departments table to database
    Co-authored-by: Eva Kemp <eva.j.kemp@gmail.com>
    Co-authored-by: Alesandria Wild <thataltgirlalesandria@gmail.com>
    Co-authored-by: Casey Hill <caseyfhill1@gmail.com>

--- UniversityRegistrar/Models/DbContext.cs
+++ UniversityRegistrar/Models/DbContext.cs
@@ -5,6 +5,7 @@ namespace UniversityRegistrar.Models
     public class UniversityRegistrarContext : DbContext
     {
         public DbSet<Student> Students {get; set;}
+        public DbSet<Department> Departments {get; set;}
         public UniversityRegistrarContext(DbContextOptions options) : base(options) { }
     }
 }
