54a43c18e344abcf974594d8d2001a73cc459c4f
Thomas Bakken <tbakken14@gmail.com>
2023-08-02 09:25:43 -0700

    Add join table for students and courses
    Co-authored-by: Eva Kemp <eva.j.kemp@gmail.com>
    Co-authored-by: Alesandria Wild <thataltgirlalesandria@gmail.com>

---
+++ UniversityRegistrar/Migrations/20230802161454_AddJoinStudentsCourses.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniversityRegistrar.Migrations
+{
+    public partial class AddJoinStudentsCourses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentsCourses",
+                columns: table => new
+                {
+                    StudentCourseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentsCourses", x => x.StudentCourseId);
+                    table.ForeignKey(
+                        name: "FK_StudentsCourses_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "CourseId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentsCourses_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentsCourses_CourseId",
+                table: "StudentsCourses",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentsCourses_StudentId",
+                table: "StudentsCourses",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentsCourses");
+        }
+    }
+}
